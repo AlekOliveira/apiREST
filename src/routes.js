04c4d4466fa463bfdd6c1cd9de9bdd5899625108
AlekOliveira <48293550+AlekOliveira@users.noreply.github.com>
2020-07-29 22:14:43 -0300

    Crud implementado por completo

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.remove);
 
 module.exports = routes;
